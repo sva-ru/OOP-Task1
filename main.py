11f33cd9a94bc3bf5e68b191b3cb00a138b8a87b
sva777 <vasya.sokolov@mail.ru>
2023-03-19 22:25:57 +0300

    task completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # {"cook_book ": [{'name': "...."}, {}, {}]}
-import collections
+# Задача №1
+import os.path
 from pprint import pprint
 with open('recipes.txt', 'rt', encoding='utf8') as file:
     cook_book = {}
@@ -17,7 +18,7 @@ with open('recipes.txt', 'rt', encoding='utf8') as file:
         file.readline()
         cook_book[dish_name] = dishes
 
-
+# Задача №2
 def get_shop_list_by_dishes(dishes, person_count):
     sl_shop_list_by_dishes = {}
     quantity = 0
@@ -32,7 +33,31 @@ def get_shop_list_by_dishes(dishes, person_count):
                     sl_shop_list_by_dishes[list(n.items())[0][1]] = {list(n.items())[2][0]: list(n.items())[2][1],list(n.items())[1][0]: quantity}
     return sl_shop_list_by_dishes
 
+def merging_files(file_to_write, folder):
+    listfiles = {}
+    path = os.getcwd() + '\\' + folder
+    for i in os.listdir(path):
+        if i.endswith('.txt') and file_to_write != i:
+            file_txt = path + '\\' + i
+            listfiles[i] = count_rows(file_txt)
+    listfiles = dict(sorted(listfiles.items(), key=lambda x: x[1][0]))
+    file_to_write = os.getcwd() + '\\' + folder + '\\' + file_to_write
+    open_file = open(file_to_write, 'a')
+    for file, it in listfiles.items():
+        count = 1
+        for str in it[1]:
+            open_file.write(f'строка № {count} в файле {file} : {str}')
+            count += 1
+        open_file.writelines('\n')
+    open_file.close()
+def count_rows(file):
+    with open(file, 'r', encoding='utf8') as f:
+        s = sum(1 for _ in f)
+        f.seek(0)
+        text = f.readlines()
+        return s, text
+
 
 pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 3))
-
-
+# в параметрах: имя итогового файла, и имя папки с исходными файлами и результирующим
+merging_files('rezult.txt', 'folder_files_txt')
